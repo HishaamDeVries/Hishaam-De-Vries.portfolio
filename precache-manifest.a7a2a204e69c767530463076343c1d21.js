ef7e0005541cf4f59fa1762f6650df59d6c6ecce
HishaamDeVries <61458881+HishaamDeVries@users.noreply.github.com>
2020-07-06 12:25:34 +0200

    Updates

---
+++ precache-manifest.a7a2a204e69c767530463076343c1d21.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8f61f44af26dd6edab742aabfaf53a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "80c8c1ccaeaca27cc77f",
+    "url": "/portfolio/static/css/2.b74fbbab.chunk.css"
+  },
+  {
+    "revision": "744e990aa9bafed65ade",
+    "url": "/portfolio/static/css/main.40c05f4a.chunk.css"
+  },
+  {
+    "revision": "80c8c1ccaeaca27cc77f",
+    "url": "/portfolio/static/js/2.fc4a0baa.chunk.js"
+  },
+  {
+    "revision": "744e990aa9bafed65ade",
+    "url": "/portfolio/static/js/main.9c076b18.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "a264306fce4bd20c316ab75223eeea4f",
+    "url": "/portfolio/static/media/ContactImg.a264306f.png"
+  },
+  {
+    "revision": "25fe56d7740e0b9b584019682de09a27",
+    "url": "/portfolio/static/media/Phone.25fe56d7.png"
+  },
+  {
+    "revision": "ccd2284e9fe66c6a4d6c7be751fb05ec",
+    "url": "/portfolio/static/media/contactKeeper.ccd2284e.png"
+  },
+  {
+    "revision": "b38ba6bdadfe9432c2e17d51a5003afb",
+    "url": "/portfolio/static/media/githubFinder.b38ba6bd.png"
+  },
+  {
+    "revision": "a1d36d9fcd4159e8771d919ed6f07758",
+    "url": "/portfolio/static/media/it-logger.a1d36d9f.png"
+  },
+  {
+    "revision": "9758fe5726a959b65d8766daa8611754",
+    "url": "/portfolio/static/media/myImage.9758fe57.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b26a0589e84cfa488f0a598ff9b7696c.js"
+  "/portfolio/precache-manifest.a7a2a204e69c767530463076343c1d21.js"
 );
 
 self.addEventListener('message', (event) => {
