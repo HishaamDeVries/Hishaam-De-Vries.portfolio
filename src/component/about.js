54a0fbd8f7cf9423fed16b8205fcd5ff4736bbf3
HishaamDeVries <61458881+HishaamDeVries@users.noreply.github.com>
2020-08-05 10:55:54 +0200

    fixed spacing problem with projects

--- src/component/about.js
+++ src/component/about.js
@@ -203,7 +203,7 @@ export default class About extends React.Component {
               <div
                 className="progress-bar fill-60-bar"
                 role="progressbar"
-                style={{ width: 65 + "%" }}
+                style={{ width: 70 + "%" }}
                 aria-valuemin="0"
                 aria-valuemax="100"
               >
